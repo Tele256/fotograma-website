d44a3ff03460e227e1a39566f92ba952fac93253
WobL23 <voicuandrei20@gmail.com>
2025-07-02 03:29:30 +0300

    preparing for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://WobL256.github.io',
 	integrations: [mdx(), sitemap()],
 	server : {
 		host: true,
 		port: 25565
-	}
+	},
 });
